efb17c711d333034ba1f274757a1374a35a0f0ce
SoTechBrasil <sotechbrasil.com@gmail.com>
2025-07-30 10:42:36 -0300

    estrutura inicial do servidor do sgm

---
+++ server/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.json()); // Para parsing de JSON
+app.use(bodyParser.urlencoded({ extended: true })); // Para parsing de formulários
+
+
+app.get('/', (req, res) => {
+    res.json({ mensagem: 'Servidor funcionando com body-parser!' });
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+    console.log(`Acesse: http://localhost:${PORT}`);
+});
