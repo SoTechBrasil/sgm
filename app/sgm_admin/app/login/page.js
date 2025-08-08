e551598d9889c865cd89a0d870f13392a0f8ddd2
SoTechBrasil <sotechbrasil.com@gmail.com>
2025-08-08 10:07:54 -0300

    adiciona validação e manipulação de login no formulário

--- app/sgm_admin/app/login/page.js
+++ app/sgm_admin/app/login/page.js
@@ -1,15 +1,39 @@
 'use client';
 import React from "react";
+import { useRouter } from "next/navigation";
 import Header from "@/components/Header/Header";
 import Container from "@/components/Container/Container";
+import login from "@/api/login";
 
 export default function Login(){
     const [dadosLogin, setDadosLogin] = React.useState({ email: "", senha: "" });
+    const router = useRouter();
 
     const handleChange = (e) => {
         const { name, value } = e.target;
         setDadosLogin((prev) => ({ ...prev, [name]: value }));
     };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(dadosLogin);
+
+        if(!dadosLogin.email || !dadosLogin.senha) {
+            alert("Por favor, preencha todos os campos.");
+            return;
+        } else {
+            const execLogin = async () => {
+                try {
+                    const response = await login(dadosLogin);
+                    console.log('Login realizado com sucesso:', response);
+                } catch (error) {
+                    console.error('Erro ao fazer login:', error);
+                }
+            };
+            execLogin();
+            router.replace("/admin");
+        }
+    }
 
     return (
         <section className="flex flex-col items-center">
@@ -19,10 +43,10 @@ export default function Login(){
             <Container styleContainer={"flex justify-center items-center flex-col min-w-[400px] max-w-[1280px] w-full h-[calc(100vh-70px)]"}>
                 <form className="flex flex-col justify-center items-left w-[50%] p-4 border border-black rounded-md text-black">
                     <label for="email">Digite o seu e-mail de admin:</label>
-                    <input type="email" className="w-full bg-blue text-black placeholder:text-black pl-2 mt-2 mb-2 border-black border rounded-md" name="emal" required onChange={handleChange} placeholder="Digite seu e-mail aqui!"/>
+                    <input type="email" className="w-full bg-blue text-black placeholder:text-black pl-2 mt-2 mb-2 border-black border rounded-md" name="email" required onChange={handleChange} placeholder="Digite seu e-mail aqui!"/>
                     <label for="senha">Digite sua senha de admin:</label>
                     <input type="password" className="w-full bg-blue text-black placeholder:text-black pl-2 mt-2 border-black border rounded-md" name="senha" required onChange={handleChange} placeholder="Digite sua senha!"/>
-                    <button type="submit" className="w-[40%] min-w-[100px] h-10 bg-red-500 mt-4 m-auto rounded-md hover:bg-red-700 duration-[800ms] text-white font-semibold text-[1.4rem]">Entrar</button>
+                    <button onClick={handleSubmit} type="submit" className="w-[40%] min-w-[100px] h-10 bg-red-500 mt-4 m-auto rounded-md hover:bg-red-700 duration-[800ms] text-white font-semibold text-[1.4rem]">Entrar</button>
                 </form>
             </Container>
         </section>
