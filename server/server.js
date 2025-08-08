350f9cd082ee69a7bf137199890b47dc5ab53a8d
SoTechBrasil <sotechbrasil.com@gmail.com>
2025-08-08 10:09:42 -0300

    habilita CORS para permitir requisições do frontend

--- server/server.js
+++ server/server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(cors({ origin: 'http://localhost:3002' })); // Habilita CORS
 app.use(bodyParser.json()); // Para parsing de JSON
 app.use(bodyParser.urlencoded({ extended: true })); // Para parsing de formulários
 app.use(require('./routes/routes'));
