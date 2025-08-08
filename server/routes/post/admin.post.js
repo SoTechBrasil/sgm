27fbe9893a5d12480b72d50d4eeeeabba63db1d4
SoTechBrasil <sotechbrasil.com@gmail.com>
2025-08-08 16:17:59 -0300

    adiciona rota de login com validação de email e senha

--- server/routes/post/admin.post.js
+++ server/routes/post/admin.post.js
@@ -1,4 +1,27 @@
 const express = require('express');
 const router = express.Router();
+const validateEmail = require('../../validate/email');
+const validatePassword = require('../../validate/password');
+
+router.post('/login', async (req, res) => {
+    try {
+        const { dataLogin } = req.body;
+
+        const emailValidation = validateEmail(dataLogin.email);
+        const passwordValidation = validatePassword(dataLogin.senha);
+
+        if (emailValidation.error) {
+            throw new Error(emailValidation.error.details[0].message);
+        } else if (passwordValidation.error) {
+            throw new Error(passwordValidation.error.details[0].message);
+        }
+
+        console.log('Dados de login recebidos:', dataLogin);
+        res.status(200).json({ message: 'Login realizado com sucesso', sucess: true });
+    } catch (error) {
+        console.error('Erro ao fazer login:', error.message);
+        res.status(400).json({ message: error.message, sucess: false });
+    }
+});
 
 module.exports = router;
