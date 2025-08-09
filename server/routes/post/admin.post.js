5c8ed1ef7b8ac8e74245fb2e2e47d3287bc6cd0e
SoTechBrasil <sotechbrasil.com@gmail.com>
2025-08-09 17:19:37 -0300

    adiciona validação de login e busca funcionário por email

--- server/routes/post/admin.post.js
+++ server/routes/post/admin.post.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const validateEmail = require('../../validate/email');
 const validatePassword = require('../../validate/password');
+const findFuncionario = require('../../db/find/funcionario');
 
 router.post('/login', async (req, res) => {
     try {
@@ -16,7 +17,13 @@ router.post('/login', async (req, res) => {
             throw new Error(passwordValidation.error.details[0].message);
         }
 
+        const funcionario = await findFuncionario.findFuncionarioByEmail(dataLogin.email);
+        if (!funcionario) {
+            throw new Error('Funcionário não encontrado');
+        }
+
         console.log('Dados de login recebidos:', dataLogin);
+        console.log('Funcionário encontrado:', funcionario);
         res.status(200).json({ message: 'Login realizado com sucesso', sucess: true });
     } catch (error) {
         console.error('Erro ao fazer login:', error.message);
