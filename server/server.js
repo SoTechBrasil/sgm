a2c2c32f6b677f9f8fd31b544d5c24a539e629e2
SoTechBrasil <sotechbrasil.com@gmail.com>
2025-08-09 09:47:31 -0300

    refatora estrutura de inicialização do servidor para incluir a conexão com o banco de dados

--- server/server.js
+++ server/server.js
@@ -1,16 +1,25 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const connectDB = require('./db/connect');
 
-const app = express();
-const PORT = process.env.PORT || 3000;
+try {
+    const app = express();
+    const PORT = process.env.PORT || 3000;
 
-app.use(cors({ origin: 'http://localhost:3002' })); // Habilita CORS
-app.use(bodyParser.json()); // Para parsing de JSON
-app.use(bodyParser.urlencoded({ extended: true })); // Para parsing de formulários
-app.use(require('./routes/routes'));
+    app.use(cors({ origin: 'http://localhost:3002' })); // Habilita CORS
 
-app.listen(PORT, () => {
-    console.log(`Servidor rodando na porta ${PORT}`);
-    console.log(`Acesse: http://localhost:${PORT}`);
-});
+    connectDB();
+
+    app.use(bodyParser.json()); // Para parsing de JSON
+    app.use(bodyParser.urlencoded({ extended: true })); // Para parsing de formulários
+    app.use(require('./routes/routes'));
+
+    app.listen(PORT, () => {
+        console.log(`Servidor rodando na porta ${PORT}`);
+        console.log(`Acesse: http://localhost:${PORT}`);
+    });
+
+} catch (e){
+    console.error('Erro ao iniciar o servidor:', e);
+}
