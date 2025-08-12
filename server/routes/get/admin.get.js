8da690af3696903b4002247083b65d3449284372
SoTechBrasil <sotechbrasil.com@gmail.com>
2025-08-12 16:31:31 -0300

    refatora a rota de obtenção de dados do administrador para incluir estrutura de dados detalhada

--- server/routes/get/admin.get.js
+++ server/routes/get/admin.get.js
@@ -6,8 +6,14 @@ router.get('/data', authenticateJWT, (req, res)=> {
     try {
         const user_admin = req.user;
         if(user_admin) {
-            console.log("Admin data fetched successfully:", user_admin);
-            res.status(200).json({ message: "Admin data fetched successfully", data: user_admin, sucess: true});
+            const data = {
+                id: user_admin.id,
+                email: user_admin.email,
+                nome: user_admin.nome,
+                role: user_admin.role
+            };
+            console.log("User data:", data);
+            res.status(200).json({ message: "Admin data fetched successfully", user_data: data, sucess: true});
         } else {
             throw new Error("Admin not found");
         }
