518bbe56aa8a2f646da4afa8a232cc6924d940b7
SoTechBrasil <sotechbrasil.com@gmail.com>
2025-08-12 15:38:46 -0300

    adiciona suporte a credenciais no CORS e corrige a importação do cookie-parser

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const cookie_parser = require('cookie-parser');
 const connectDB = require('./db/connect');
 const gerarOuCarregarChave = require('./service/gerarOuCarregarChave');
 require('dotenv').config();
@@ -9,13 +10,14 @@ try {
     const app = express();
     const PORT = process.env.PORT || 3000;
 
-    app.use(cors({ origin: 'http://localhost:3002' })); // Habilita CORS
+    app.use(cors({ origin: 'http://localhost:3002', credentials: true })); // Habilita CORS
 
     connectDB();
     const jwtSecret = gerarOuCarregarChave();
 
     app.use(bodyParser.json()); // Para parsing de JSON
     app.use(bodyParser.urlencoded({ extended: true })); // Para parsing de formulários
+    app.use(cookie_parser()); // Para parsing de cookies
     app.use(require('./routes/routes'));
 
     app.listen(PORT, () => {
