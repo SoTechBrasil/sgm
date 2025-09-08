f404a542af8d0076a6b2d4773257ab00374af80f
SoTechBrasil <sotechbrasil.com@gmail.com>
2025-09-08 14:31:24 -0300

    adiciona rota para listar mesas com tratamento de erros

--- server/routes/get/public.get.js
+++ server/routes/get/public.get.js
@@ -1,8 +1,27 @@
 const express = require('express');
 const router = express.Router();
+const Mesa = require('../../models/Mesas');
 
 router.get('/', (req, res) => {
     res.json({ mensagem: 'Servidor funcionando com body-parser!' });
+});
+
+router.get('/list/mesas', async (req, res) => {
+    try {
+        const mesas = await Mesa.find();
+        const mesasFiltradas = mesas.map(mesa => {
+            return {
+                mesa_name: mesa.table_name,
+                mesa_status: mesa.status,
+                capacidade_max: mesa.capacidade
+            }
+        })
+        
+        res.json(mesasFiltradas);
+    } catch (e) {
+        console.error(e);
+        res.status(500).json({ mensagem: 'Erro ao listar mesas.' });
+    }
 });
 
 module.exports = router;
