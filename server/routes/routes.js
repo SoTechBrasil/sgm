9c45ac4e34ffec7a5624371ba3e103c58e4e44aa
wilsonsilvadeveloper <wilsonoficial.com@gmail.com>
2025-09-11 15:58:12 -0300

    organiza as rotas, movendo as importações e ajustando a ordem das definições

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -2,22 +2,25 @@ const express = require('express');
 const router = express.Router();
 const authenticateJWT = require('../middleware/authentication');
 
+// Rotas Get
+const public_routes_get = require('./get/public.get');
 const garcon_routes_get = require('./get/garcon.get');
 const admin_routes_get = require('./get/admin.get');
-const public_routes_get = require('./get/public.get');
-
-const garcon_routes_post = require('./post/garcon.post');
-const admin_routes_post = require('./post/admin.post');
-const public_routes_post = require('./post/public.post');
-
-// Rotas Get
-router.use('/', authenticateJWT, public_routes_get);
-router.use('/garcon', garcon_routes_get);
-router.use('/admin', admin_routes_get);
 
 // Rotas Post
+const public_routes_post = require('./post/public.post');
+const login_routes_post = require('./post/login');
+const garcon_routes_post = require('./post/garcon.post');
+const admin_routes_post = require('./post/admin.post');
+
+router.use('/', public_routes_get);
 router.use('/', public_routes_post);
-router.use('/garcon', garcon_routes_post);
-router.use('/admin', admin_routes_post);
+router.use('/login', login_routes_post);
+
+router.use('/garcon', authenticateJWT, garcon_routes_get);
+router.use('/garcon', authenticateJWT, garcon_routes_post);
+
+router.use('/admin', authenticateJWT, admin_routes_get);
+router.use('/admin', authenticateJWT, admin_routes_post);
 
 module.exports = router;
